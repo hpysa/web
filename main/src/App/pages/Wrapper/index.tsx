c53cb9debb5afcf959dc967dd918e80cd8b755e8
Joshua Nilsson <27570195+joshua-nilsson@users.noreply.github.com>
2025-02-15 20:59:03 -0700

    Deploy --- 02/15/2025  8:59:03 PM

--- main/src/App/pages/Wrapper/index.tsx
+++ main/src/App/pages/Wrapper/index.tsx
@@ -5,7 +5,7 @@ import Footer from '../../components/Footer';
 const Wrapper = () => (
 	<section className="max-w-4xl flex flex-col mx-auto gap-8 px-4 lg:px-0">
 		<Header />
-		<main className="flex flex-col gap-8">
+		<main className="flex flex-col gap-8 mt-8 sm:mt-0">
 			<Outlet />
 		</main>
 		<Footer />
