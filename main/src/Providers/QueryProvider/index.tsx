173c05e5ee9eb97bacc55ef9e3d69b39234ea6c2
Joshua Nilsson <27570195+joshua-nilsson@users.noreply.github.com>
2025-03-30 11:12:51 -0600

    Deploy --- 03/30/2025 11:12:51 AM

--- main/src/Providers/QueryProvider/index.tsx
+++ main/src/Providers/QueryProvider/index.tsx
@@ -1,11 +1,16 @@
 import ProviderType from '@/types/providerType';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { memo } from 'react';
 
 const QueryProvider = memo(({ children }: ProviderType) => {
     const queryClient = new QueryClient();
-
-    return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+            <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" position="left" />
+        </QueryClientProvider>
+    );
 });
 
 export default QueryProvider;
