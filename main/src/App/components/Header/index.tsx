197a3955c2f0b27c3cdd8167eb16c8502f1e5f0d
Joshua Nilsson <27570195+joshua-nilsson@users.noreply.github.com>
2025-03-30 10:19:32 -0600

    Deploy --- 03/30/2025 10:19:32 AM

--- main/src/App/components/Header/index.tsx
+++ main/src/App/components/Header/index.tsx
@@ -39,7 +39,7 @@ const Header = () => (
             <section className="text-md mx-auto grid w-full grid-cols-1 grid-rows-2 gap-6 sm:grid-cols-[repeat(2,minmax(15rem,1fr))] lg:gap-8">
                 <Card className="border-0">
                     <div className="flex flex-col items-center justify-center gap-1 rounded border p-4 font-sans text-base uppercase text-primary shadow-md sm:p-4">
-                        <span className="font-medium uppercase">BISHOP MUGLESTON</span>
+                        <span className="font-medium text-lg">BISHOP MUGLESTON</span>
                         <a className="text-tertiary transition-all hover:text-tertiary hover:underline" href="tel:(801) 367-0602" aria-label="Bishop's Phone Number">
                             (801) 367-0602
                         </a>
@@ -47,7 +47,7 @@ const Header = () => (
                 </Card>
                 <Card className="border-0">
                     <div className="flex flex-col items-center justify-center gap-1 rounded border p-4 font-sans text-base uppercase text-primary shadow-md sm:p-4">
-                        <span className="font-medium">EXECUTIVE SECRETARY</span>
+                        <span className="font-medium text-lg">EXECUTIVE SECRETARY</span>
                         <a className="text-tertiary transition-all hover:text-tertiary hover:underline" href="tel:(209) 790-9348" aria-label="Executive Secretary's Phone Number">
                             (209) 790-9348
                         </a>
@@ -55,13 +55,13 @@ const Header = () => (
                 </Card>
                 <Card className="border-0">
                     <div className="flex flex-col items-center justify-center gap-1 rounded border p-4 font-sans text-base uppercase text-primary shadow-md sm:p-4">
-                        <span className="font-medium">ACTIVITY NIGHT</span>
+                        <span className="font-medium text-lg">ACTIVITY NIGHT</span>
                         <span>Mondays @ 7:00PM</span>
                     </div>
                 </Card>
                 <Card className="border-0">
                     <div className="flex flex-col items-center justify-center gap-1 rounded border p-4 font-sans text-base uppercase text-primary shadow-md sm:p-4">
-                        <span className="font-medium">STAKE NIGHT</span>
+                        <span className="font-medium text-lg">STAKE NIGHT</span>
                         <span>Thursdays @ 6:30PM</span>
                     </div>
                 </Card>
