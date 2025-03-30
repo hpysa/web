15164547eeb7133933f16673e5974490c85a0f66
Joshua Nilsson <27570195+joshua-nilsson@users.noreply.github.com>
2025-03-30 10:55:38 -0600

    Deploy --- 03/30/2025 10:55:38 AM

--- main/src/App/components/Footer/index.tsx
+++ main/src/App/components/Footer/index.tsx
@@ -9,7 +9,7 @@ const Footer = () => (
                     index
                     element={
                         <a className="transition-all hover:text-tertiary hover:underline" href="/" aria-label="Heritage Park YSA">
-                            Heritage Park YSA
+                            Heritage Park YSA Ward, 26–35
                         </a>
                     }
                 />
